d611350be3c1030adefe621787b65faa85139577
Jon Taylor <jontaylor224@gmail.com>
2019-12-02 16:08:50 -0600

    edited form for clarity of Boast/Roast checkbox

--- posts/forms.py
+++ posts/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(forms.Form):
     content = forms.CharField(max_length=280, label='Make your post here')
     is_boast = forms.BooleanField(
-        label='Check here to Boast! ', required=False)
+        label='Click to make a Boast; Leave empty for a Roast! ', required=False)
